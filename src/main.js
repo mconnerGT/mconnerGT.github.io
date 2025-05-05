b82fa0683cbb98c863c4ec5f0fa9c3d110d8fed7
Matthew Conner <mcconner@gatech.edu>
2025-05-05 00:41:50 -0400

    Map now loads centered on location, if available

--- src/main.js
+++ src/main.js
@@ -2,47 +2,64 @@ import maplibregl from "maplibre-gl";
 import circleToPolygon from "circle-to-polygon";
 import "maplibre-gl/dist/maplibre-gl.css";
 
-
-const map = new maplibregl.Map({
-    container: 'map',
-    style: 'https://tiles.openfreemap.org/styles/liberty', // A default tile source
-    center: [0, 0], // Default center coordinates (longitude, latitude)
-    zoom: 2 // Default zoom level
-});
-
-// Add controls
-map.addControl(new maplibregl.NavigationControl({
-    showZoom: true,  // Enables zoom buttons
-    showCompass: true // Enables the compass
-}));
-map.addControl(new maplibregl.GeolocateControl({
-    positionOptions: { enableHighAccuracy: true },
-    trackUserLocation: true // Keeps tracking the user’s movement
-}));
-map.addControl(new maplibregl.FullscreenControl());
-map.addControl(new maplibregl.ScaleControl());
-
-const center = [-83.2393, 42.7845];
-const radius = 1609; // 1 mile in meters
-
-const polygon = circleToPolygon(center, radius);
-
-map.on('load', () => {
-    
-    // Add a GeoJSON source for the circle
-    map.addSource('circle-source', {
-        type: 'geojson',
-        data: { type: "Feature", geometry: polygon }
+function initializeMap(mapCenter, zoom) {
+    const map = new maplibregl.Map({
+        container: 'map',
+        style: 'https://tiles.openfreemap.org/styles/liberty', // A default tile source
+        center: mapCenter,
+        zoom: zoom
     });
 
-    // Add a layer to render the circle
-    map.addLayer({
-        id: 'circle-layer',
-        type: 'fill',
-        source: 'circle-source',
-        paint: {
-            'fill-color': '#ff0000', // Red color
-            'fill-opacity': 0.5
-        }
+    map.on('load', () => {
+        // Add controls
+        map.addControl(new maplibregl.NavigationControl({
+            showZoom: true,  // Enables zoom buttons
+            showCompass: true // Enables the compass
+        }));
+        map.addControl(new maplibregl.GeolocateControl({
+            positionOptions: { enableHighAccuracy: true },
+            trackUserLocation: true // Keeps tracking the user’s movement
+        }));
+        map.addControl(new maplibregl.FullscreenControl());
+        map.addControl(new maplibregl.ScaleControl());
+
+        // Add a GeoJSON source for the circle
+        const cirCenter = [-83.2393, 42.7845];
+        const radius = 1609; // 1 mile in meters
+        const polygon = circleToPolygon(cirCenter, radius);
+        
+        map.addSource('circle-source', {
+            type: 'geojson',
+            data: { type: "Feature", geometry: polygon }
+        });
+
+        // Add a layer to render the circle
+        map.addLayer({
+            id: 'circle-layer',
+            type: 'fill',
+            source: 'circle-source',
+            paint: {
+                'fill-color': '#ff0000', // Red color
+                'fill-opacity': 0.5
+            }
+        });
     });
-});
+}
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+        (position) => {            
+            if(!isNaN(position?.coords?.latitude) || !isNaN(position?.coords?.longitude)) {
+                initializeMap([position.coords.longitude, position.coords.latitude], 10);
+            }
+            else
+            {
+                // TODO - is there a way to combine this with the other failure case?
+                initializeMap([0,0], 2);
+            }
+    })
+}
+else
+{
+    initializeMap([0,0], 2);
+}
