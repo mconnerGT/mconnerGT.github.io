9f8d1908c2dcb42cc143d03be96c4ef9c7cd1675
Matthew Conner <mcconner@gatech.edu>
2025-05-04 23:44:55 -0400

    removed unecessary prefix from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "mconnerGT.github.io",
+    base: "/",
     build: {
         outDir: "dist",
         assetsDir: "assets",
